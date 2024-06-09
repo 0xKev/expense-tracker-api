34ce87531e68dca1c1a5e950fb970d7a3fbd2d1b
0xKev <56137695+0xKev@users.noreply.github.com>
2024-06-08 21:08:14 -0400

    feat(Expense): purchase_date field default value set to timezone.now().date()

--- expense_tracker_project/expense_tracker/models.py
+++ expense_tracker_project/expense_tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import CustomUser
+from django.utils import timezone
 
 # Create your models here.
+class Expense(models.Model):
+    CATEGORY_CHOICES = [
+        ('GROCERIES', 'Groceries'),
+        ('LEISURE', 'Leisure'),
+        ('ELECTRONICS', 'Electronics'),
+        ('UTILITIES', 'Utilities'),
+        ('CLOTHING', 'Clothing'),
+        ('HEALTH', 'Health'),
+        ('OTHERS', 'Others'),
+    ]
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    purchase_date = models.DateField("expense date", default=timezone.now().date())
+    amount = models.IntegerField("amount spent")
+    purchase_type = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    other_purchase_type = models.CharField(max_length=20, blank=True, null=True)
