fd2298239e294d2169a7751bcaf57be6622803e4
0xKev <56137695+0xKev@users.noreply.github.com>
2024-07-02 14:08:02 -0400

    tests: Add TestCase for creating accounts

--- expense_tracker_project/accounts/tests.py
+++ expense_tracker_project/accounts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth import get_user_model
+
+
+class CustomUsersTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create(
+            email="foo@gmail.com",
+            username="foo_user",
+            password="foo_pass"
+        )
+        self.assertEqual(user.email, "foo@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+
+class ExpenseTest(TestCase):
+    pass
