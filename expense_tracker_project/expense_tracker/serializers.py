cb49ce1878e9eb27aeb4432d08e988e0ef893556
0xKev <56137695+0xKev@users.noreply.github.com>
2024-07-02 14:03:34 -0400

    add(CustomUserSerializer): Add CustomUserSerializer for CustomUser model

--- expense_tracker_project/expense_tracker/serializers.py
+++ expense_tracker_project/expense_tracker/serializers.py
@@ -1,9 +1,27 @@
 from accounts.models import CustomUser
 from rest_framework import serializers, permissions
 from .models import *
+from django.contrib.auth.hashers import make_password
 
 class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Expense
         exclude = ["owner"]
-        permissions_class = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "password"]
+
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            username = validated_data["username"],
+            email = validated_data["email"],
+            password = make_password(validated_data["password"])
+        )
+        return user
+    
+
