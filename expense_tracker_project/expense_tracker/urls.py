573c076c637f37e75a9495714429d6dc1dddab53
0xKev <56137695+0xKev@users.noreply.github.com>
2024-07-02 14:04:56 -0400

    feat: Add api endpoints for obtaining token, token refresh and schema view

--- expense_tracker_project/expense_tracker/urls.py
+++ expense_tracker_project/expense_tracker/urls.py
@@ -2,12 +2,25 @@ from django.urls import path, include
 from .views import *
 
 from rest_framework import routers
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
 
 
 router = routers.DefaultRouter()
 router.register(r'expense', ExpenseViewSet, basename="expense")
 
+# added just to have api view of both cbv and fbv
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response({
+        "expenses": reverse("expense-list", request=request, format=format),
+        "sign-up": reverse("sign-up", request=request, format=format)
+    })
+
+
 urlpatterns = [
+    path("", api_root, name="api-root"),
     path("", include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("sign-up/", SignUpView.as_view(), name="sign-up")
 ]
