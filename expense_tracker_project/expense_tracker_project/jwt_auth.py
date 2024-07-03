67afbe5aa34beca3ed9dd6d041c4cd17120912c1
0xKev <56137695+0xKev@users.noreply.github.com>
2024-07-03 13:23:48 -0400

    add: Add JWT View definitions to generate OpenAPI Schemas

---
+++ expense_tracker_project/expense_tracker_project/jwt_auth.py
@@ -0,0 +1,96 @@
+from rest_framework import serializers, status
+from drf_spectacular.utils import extend_schema, OpenApiExample
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+    
+@extend_schema(
+        description="Obtain JWT token pair",
+        responses={
+            200: OpenApiExample(
+                'Successful Response',
+                value={
+                    'access': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+                    'refresh': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+                },
+                response_only=True,
+            )
+        },
+        examples=[
+            OpenApiExample(
+                'Example Request',
+                value={'username': 'user@example.com', 'password': 'password123'},
+                request_only=True,
+            )
+        ]
+)
+class TokenObtainPairResponseSerializer(serializers.Serializer):
+    access = serializers.CharField()
+    refresh = serializers.CharField()
+
+    def create(self, validated_data):
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
+
+
+class DecoratedTokenObtainPairView(TokenObtainPairView):
+    @extend_schema(
+        responses={
+            status.HTTP_200_OK: TokenObtainPairResponseSerializer,
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class TokenRefreshResponseSerializer(serializers.Serializer):
+    access = serializers.CharField()
+
+    def create(self, validated_data):
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
+
+
+class DecoratedTokenRefreshView(TokenRefreshView):
+    @extend_schema(
+        responses={
+            status.HTTP_200_OK: TokenRefreshResponseSerializer,
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class TokenVerifyResponseSerializer(serializers.Serializer):
+    def create(self, validated_data):
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
+
+
+class DecoratedTokenVerifyView(TokenVerifyView):
+    @extend_schema(
+        responses={
+            status.HTTP_200_OK: TokenVerifyResponseSerializer,
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class TokenBlacklistResponseSerializer(serializers.Serializer):
+    def create(self, validated_data):
+        raise NotImplementedError()
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError()
+
+
